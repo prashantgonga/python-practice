2b77351abc0e11689eb3d4044caf31fff4b5c211
prashantgonga <prashant.gonga@gmail.com>
2020-11-17 20:44:05 +0800

    Simple program using function to greet in different languages

---
+++ course1/functions/greetings.py
@@ -0,0 +1,11 @@
+# Using functions to greet in different languages
+
+def greet(lang) : 
+    if lang == 'fr' :
+        print('Bonjour') 
+    elif lang == 'es' :
+        print('Hola')
+    else :
+        print('Hello')
+    
+greet('potato')
