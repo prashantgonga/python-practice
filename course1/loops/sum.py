d00d8b96f82ac846e0b31f770b02cd08630405df
prashantgonga <prashant.gonga@gmail.com>
2020-11-17 23:53:28 +0800

    Simple program to count and calculate sum using for loop

---
+++ course1/loops/sum.py
@@ -0,0 +1,11 @@
+# Simple progam to count and calculate sum using for loop
+
+counter = 0
+print('Before:', counter)
+
+for number in [9, 41, 12, 3, 74, 15] :
+    counter = counter + number
+    print(counter, number)
+
+print('After', counter)
+print('All done!')
