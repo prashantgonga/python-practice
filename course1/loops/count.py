cc1d14fbb45821647a0328377264a3efa4095594
prashantgonga <prashant.gonga@gmail.com>
2020-11-17 23:49:06 +0800

    Simple program to count using for loop

---
+++ course1/loops/count.py
@@ -0,0 +1,11 @@
+# Program to count using for loop
+
+counter = 0
+print('Before:', counter)
+
+for number in [9, 41, 12, 3, 74, 15] :
+    counter = counter + 1
+    print(counter, number)
+
+print('After', counter)
+print('All done!')
