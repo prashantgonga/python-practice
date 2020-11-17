6033cda659da4927c79b94a4e6d535cbd4101078
prashantgonga <prashant.gonga@gmail.com>
2020-11-17 23:43:47 +0800

    Finding the largest number in the list using for loop

---
+++ course1/loops/largest.py
@@ -0,0 +1,11 @@
+# Finding the largest number using for loop
+
+largest_so_far = -1 
+print('Before ', largest_so_far)
+
+for number in [9, 41, 12, 3, 74, 15] :
+    if number > largest_so_far :
+        largest_so_far = number
+    print(largest_so_far)
+
+print('All done!')
