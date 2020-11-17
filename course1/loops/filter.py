4df48afbfac57312982f6ff81577616d9a763eb1
prashantgonga <prashant.gonga@gmail.com>
2020-11-18 00:03:37 +0800

    Simple program to filter values using for loop

---
+++ course1/loops/filter.py
@@ -0,0 +1,9 @@
+# Simple program to filter values using for loop
+
+print('Before')
+for number in [9, 41, 12, 3, 74, 15] :
+    if number > 20 :
+        print(number)
+
+print('After')
+print('All Done!')
