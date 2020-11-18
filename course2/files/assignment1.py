fffc535b470fca47d820db6773a346902fdbdc24
prashantgonga <prashant.gonga@gmail.com>
2020-11-18 18:38:33 +0800

    assignment1

---
+++ course2/files/assignment1.py
@@ -0,0 +1,16 @@
+# 7.1 Write a program that prompts for a file name, then opens that file and reads through the file, 
+# and print the contents of the file in upper case. 
+# Use the file words.txt to produce the output below.
+
+fileName = input('Enter a file name: ')
+
+try : 
+    fileHandle = open(fileName, 'r')
+except :
+    print(fileName, 'Cannot be opened')
+    quit()
+
+for lines in fileHandle :
+    format = lines.upper()
+    result = format.rstrip()
+    print(result)
