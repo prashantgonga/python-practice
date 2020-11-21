c07a921924915c8c66027a6af4f54663c73bd5f1
prashantgonga <prashant.gonga@gmail.com>
2020-11-21 23:06:03 +0800

    Using beautiful soup to crawl websites

---
+++ course3/network/bsoup2.py
@@ -0,0 +1,22 @@
+# Using beautiful soup to crawl websites
+
+# Import urllib library and beautifulsoup
+import urllib.request, urllib.parse, urllib.error
+from bs4 import BeautifulSoup
+import ssl # this is sometimes required for ssl enabled sites
+
+# Ignore SSL certificates error
+
+ctx = ssl.create_default_context()
+ctx.check_hostname = False
+ctx.verify_mode = ssl.CERT_NONE
+
+url = input('Enter a URL to crawl: ')
+html = urllib.request.urlopen(url, context=ctx).read()
+soup = BeautifulSoup(html, 'html.parser')
+
+# Retrieve all of the anchor tags 
+
+tags = soup('a')
+for tag in tags :
+    print(tag.get('href', None))
